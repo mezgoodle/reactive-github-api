0b8e64769ad2de38c4279375f5d2511f6fe8e78e
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-23 14:16:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,17 +16,18 @@ class App extends React.Component {
 	gettingInfo = async (e) => {
 		e.preventDefault();
 		const user = e.target.elements.user.value;
-		const api_url = await fetch(`https://api.github.com/users/${user}`);
-		const data = await api_url.json();
-		console.log(data)
-
-		this.setState({
-			login: data.login,
-			html_url: data.html_url,
-			public_repos: data.public_repos,
-			url: data.url,
-			error: '',
-		});
+		
+		if (user) {
+			const api_url = await fetch(`https://api.github.com/users/${user}`);
+			const data = await api_url.json();
+			this.setState({
+				login: data.login,
+				html_url: data.html_url,
+				public_repos: data.public_repos,
+				url: data.url,
+				error: '',
+			});	
+		}
 	}
 
 	render() {
